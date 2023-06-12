55b49e3ec8ca3d8b221f6f88b7b46bb7060fb690
andy <aztimebytes@gmail.com>
2023-06-11 22:40:55 -0400

    routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const blogpostRoutes = require("./blogpost");
+const commentRoutes = require("./comment");
+const userRoutes = require("./user");
+
+router.use("/blogpost", blogpostRoutes);
+router.use("/comment", commentRoutes);
+router.use("/login", userRoutes);
 
 module.exports = router;
